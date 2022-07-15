03e785abccf8d349c4295201b77a421e008e8a44
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-07-14 19:14:51 -0700

    Added param reqs and while loop delay

--- Text Randomizer/C++/main.cpp
+++ Text Randomizer/C++/main.cpp
@@ -6,12 +6,45 @@
 
 using namespace std;
 
+void getPrintParams(int& perSec, int& secs, int& length, bool& format, ostream& output, istream& input);
+void printText(int length, bool format, ostream& output);
+
 int main(){
     
     // Ask user for per sec + how many secs + how long each random string is + string format
+    int perSec, secs, length;
+    bool format = true;
+
+    getPrintParams(perSec, secs, length, format, cout, cin);
+
+    int timeout = 60000/perSec;
 
     // Print
+    while(true){
+
+        printText(length, format, cout);
+         
+        std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
+    }
     
+}
+
+void getPrintParams(int& perSec, int& secs, int& length, bool& format, ostream& output, istream& input){
+
+    output << "Input # of strings per second to be printed: ";
+    input >> perSec;
+
+    output << "Input # of secs (duration): ";
+    input >> secs;
+
+    output << "Input string length: ";
+    input >> length;
+
+    output << "Would you like strings (input 0) or JSON (input 1): ";
+    input >> format;
+
+    output << '\n';
+
 }
 
 void printText(int length, bool format, ostream& output){
@@ -29,6 +62,7 @@ void printText(int length, bool format, ostream& output){
         outputString = temp;
     }
 
-    output << ctime(&timenow) << ": " << outputString;
+    output << ctime(&timenow);
+    output << "> " << outputString << "\n\n";
 
 }
